5cfa71c74a40a6f825c92bcc7267520031f80df3
hmasusi <necromandus@posteo.mx>
2026-05-16 11:06:56 -0500

    emacs serivce

---
+++ emacs-service.rb
@@ -0,0 +1,10 @@
+class emacsService < Formula
+  desc "Emacs server service"
+  homepage "https://www.gnu.org/software/emacs/"
+  service do
+    run [opt_bin/"emacs", "--fg-daemon"]
+    keep_alive true
+    log_path "/tmp/homebrew.mxcl.emacs-plus.stdout.log"
+    error_log_path "/tmp/homebrew.mxcl.emacs-plus.stderr.log"
+  end
+end
